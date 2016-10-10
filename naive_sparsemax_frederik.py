efc8f286c413d422fadb4a8cc3166332fbc058a6
frederik wolgast rørbech <frederik.ubuntu.wolgast@gmail.com>
2016-10-10 15:47:44 +0200

    added loss function

--- naive_sparsemax_frederik.py
+++ naive_sparsemax_frederik.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def sparsemax(z_array):
+def sparsemax(z_array, return_tau=False):
     """
     Computes the sparsemax of an independent
 
@@ -17,6 +17,7 @@ def sparsemax(z_array):
     sorted_z = np.sort(z_array, axis=1)[:, ::-1]
 
     p_array = np.empty(z_array.shape)
+    tau_vector = np.empty((z_array.shape[0], 1))
     for row in range(0, z_array.shape[0]):
         max_k = np.NaN
 
@@ -29,12 +30,15 @@ def sparsemax(z_array):
 
         # step 3: define tau
         tau_z = (np.sum(sorted_z[row, :max_k]) - 1) / max_k
-
+        tau_vector[row] = tau_z
         # step 4: set sparse probability vector
         p_array[row,:] = np.maximum(z_array[row, :] - tau_z, 0)
 
     #return array
-    return p_array
+    if return_tau:
+        return [p_array, tau_vector]
+    else:
+        return p_array
 
 def jacobian_sparsemax(p_vector):
     """
@@ -53,16 +57,31 @@ def jacobian_sparsemax_times_vector(p_vector, v_vector):
 
     return np.multiply(s, v_vector - v_hat)
 
+def loss_sparsemax(z_vector, p_vector, target_label, tau_threshold):
+    s = p_vector > 0
+    return -z_vector[target_label] + 0.5 * np.sum(np.power(z_vector[s], 2) - tau_threshold ** 2 ) + 0.5
 
 oned_test = np.array([[5, 3, 0]])
 twod_test = np.array([[5, 3, 0], [2, 2.7, 0]])
+twod_targets = np.array([0, 1])
+
+print("trying to evaluate sparsemax")
 print(sparsemax(oned_test))
 print(sparsemax(twod_test))
 
+print("\nTesting jacobian")
 for i, row in enumerate(sparsemax(twod_test)):
-    print(i)
-    print(twod_test[i, :])
-    print(row)
-    print(jacobian_sparsemax(row))
-    print(jacobian_sparsemax_times_vector(row, twod_test[i, :]))
-    print(np.dot(jacobian_sparsemax(row), twod_test[i, :]))
+    print("\ntesting row {0}".format(i))
+    print("z: ", twod_test[i, :])
+    print("sparse p: ", row)
+    print("jacobian: ", jacobian_sparsemax(row))
+    print("jacobian * z (indirectly): ", jacobian_sparsemax_times_vector(row, twod_test[i, :]))
+    print("jacobian * z (directly): ", np.dot(jacobian_sparsemax(row), twod_test[i, :]))
+
+[p_array, taus] = sparsemax(twod_test, return_tau=True)
+print("\ntesting loss")
+for i, tau in enumerate(taus):
+    print("z:", twod_test[i, :])
+    print("predicted: ", p_array[i, :])
+    print("target: ", twod_targets[i])
+    print("loss: ", loss_sparsemax(twod_test[i, :], p_array[i, :], twod_targets[i], tau))
