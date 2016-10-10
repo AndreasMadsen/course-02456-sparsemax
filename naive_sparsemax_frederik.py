76cc448adde676f9c1c6d3ef16981dbdaa18a6a6
frederik wolgast rørbech <frederik.ubuntu.wolgast@gmail.com>
2016-10-10 15:05:20 +0200

    implemented jacobian and jacobian times vector

--- naive_sparsemax_frederik.py
+++ naive_sparsemax_frederik.py
@@ -19,6 +19,7 @@ def sparsemax(z_array):
     p_array = np.empty(z_array.shape)
     for row in range(0, z_array.shape[0]):
         max_k = np.NaN
+
         # step 2: find argmax_k
         for k in range(1, z_array.shape[1]):
             if 1 + k * sorted_z[row, k - 1] > np.sum(sorted_z[row, :k]):
@@ -35,7 +36,33 @@ def sparsemax(z_array):
     #return array
     return p_array
 
+def jacobian_sparsemax(p_vector):
+    """
+    Computes the jacobian of a sparsemax probability vector
+    """
+
+    # compute vector with i=1 if i in S(z)
+    s = p_vector > 0
+    jacobian = np.diag(s)  - np.outer(s, s.T) / np.sum(s)
+
+    return jacobian
+
+def jacobian_sparsemax_times_vector(p_vector, v_vector):
+    s = p_vector > 0
+    v_hat = np.sum(v_vector[s]) / (np.sum(s))
+
+    return np.multiply(s, v_vector - v_hat)
+
+
 oned_test = np.array([[5, 3, 0]])
 twod_test = np.array([[5, 3, 0], [2, 2.7, 0]])
 print(sparsemax(oned_test))
 print(sparsemax(twod_test))
+
+for i, row in enumerate(sparsemax(twod_test)):
+    print(i)
+    print(twod_test[i, :])
+    print(row)
+    print(jacobian_sparsemax(row))
+    print(jacobian_sparsemax_times_vector(row, twod_test[i, :]))
+    print(np.dot(jacobian_sparsemax(row), twod_test[i, :]))
