68e379695e7b911ff0dbac53360c2de9db6a8525
Andreas Madsen <amwebdk@gmail.com>
2016-10-21 22:08:12 +0200

    add gpu kernel for x^2 (not working)

--- tensorflow-example/kernel/custom_square.cc
+++ tensorflow-example/kernel/custom_square.cc
@@ -16,9 +16,9 @@ REGISTER_OP("CustomSquare")
       return Status::OK();
     });
 
-class CustomSquareOp : public OpKernel {
+class CustomSquareCPUOp : public OpKernel {
  public:
-  explicit CustomSquareOp(OpKernelConstruction* context) : OpKernel(context) {}
+  explicit CustomSquareCPUOp(OpKernelConstruction* context) : OpKernel(context) {}
 
   void Compute(OpKernelContext* context) override {
     // Grab the input tensor
@@ -32,7 +32,7 @@ class CustomSquareOp : public OpKernel {
     auto output = output_tensor->flat<int32>();
 
     // output = input * input
-    printf("> debug: c++ op\n");
+    printf("> debug: c++ cpu-op\n");
     const int N = input.size();
     for (int i = 0; i < N; i++) {
       output(i) = input(i) * input(i);
@@ -40,4 +40,31 @@ class CustomSquareOp : public OpKernel {
   }
 };
 
-REGISTER_KERNEL_BUILDER(Name("CustomSquare").Device(DEVICE_CPU), CustomSquareOp);
+REGISTER_KERNEL_BUILDER(Name("CustomSquare").Device(DEVICE_CPU), CustomSquareCPUOp);
+
+#if GOOGLE_CUDA
+void CustomSquareKernelLauncher(const int* in, const int N, int* out);
+
+class CustomSquareGPUOp : public OpKernel {
+ public:
+  explicit CustomSquareGPUOp(OpKernelConstruction* context) : OpKernel(context) {}
+
+  void Compute(OpKernelContext* context) override {
+    // Grab the input tensor
+    const Tensor& input_tensor = context->input(0);
+    auto input = input_tensor.flat<int32>();
+
+    // Create an output tensor
+    Tensor* output_tensor = NULL;
+    OP_REQUIRES_OK(context, context->allocate_output(0, input_tensor.shape(),
+                                                     &output_tensor));
+    auto output = output_tensor->template flat<int32>();
+
+    const int N = input.size();
+    printf("> debug: c++ gpu-op\n");
+    CustomSquareKernelLauncher(input.data(), N, output.data());
+  }
+};
+
+REGISTER_KERNEL_BUILDER(Name("CustomSquare").Device(DEVICE_GPU), CustomSquareGPUOp);
+#endif
