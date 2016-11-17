bd686657c3537442669f38378806b018df591886
Andreas Madsen <amwebdk@gmail.com>
2016-11-17 15:32:38 +0100

    add seperate CPU and GPU sparsemax regressors

--- benchmark/regressors.py
+++ benchmark/regressors.py
@@ -1,15 +1,47 @@
 
+import os
+
 import tensorflow_python
 import tensorflow_sparsemax
 import python_reference
 import tensorflow_softmax
 
+CUDA_VISIBLE_DEVICES = None
+if 'CUDA_VISIBLE_DEVICES' in os.environ:
+    CUDA_VISIBLE_DEVICES = os.environ['CUDA_VISIBLE_DEVICES']
+
+
+class SparsemaxRegressionNativeCPU(tensorflow_sparsemax.SparsemaxRegression):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.name += ' CPU'
+
+    def __enter__(self):
+        if CUDA_VISIBLE_DEVICES is not None:
+            os.environ['CUDA_VISIBLE_DEVICES'] = ''
+        return super().__enter__()
+
+    def __exit__(self, *args):
+        if CUDA_VISIBLE_DEVICES is not None:
+            os.environ['CUDA_VISIBLE_DEVICES'] = CUDA_VISIBLE_DEVICES
+        return super().__exit__(*args)
+
+
+class SparsemaxRegressionNativeGPU(tensorflow_sparsemax.SparsemaxRegression):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.name += ' GPU'
+
 all_regressors = [
     tensorflow_python.SparsemaxRegression,
-    tensorflow_sparsemax.SparsemaxRegression,
+    SparsemaxRegressionNativeCPU,
+    SparsemaxRegressionNativeGPU,
     python_reference.SparsemaxRegression,
     tensorflow_softmax.SoftmaxRegression
 ]
+
+if CUDA_VISIBLE_DEVICES is None:
+    all_regressors.remove(SparsemaxRegressionNativeGPU)
 
 data_regressors = [
     tensorflow_sparsemax.SparsemaxRegression,
